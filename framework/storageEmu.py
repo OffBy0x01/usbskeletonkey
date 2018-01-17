2343096293a1c651cc247805d14c1f6b3241e8de
forbes <1500812@abertay.ac.uk>
2018-01-17 13:43:39 +0000

    Imports

--- framework/storageEmu.py
+++ framework/storageEmu.py
@@ -3,18 +3,19 @@ This file is to load a file system onto the connected host.
 
 This file will have an accompanying file system to clone for each run.
 
-This file system could also be loaded via loopback to save the outputs of modules
+This file system could also be loaded via loop back to save the outputs of modules
 '''
 
-import FwComponent
+from framework import FwComponent
 
 class storageAccess(fw_component_dr)
 
-    def __init__(self, other):
-        #Copy the template to make a little memory file for this run
+    def __init__(self, size, other):
+        #Create device to store to
+        self.size = approximate_size(size)
 
     def mountLocal(self, directory):
-        #Mount the file system locally for ammending
+        #Mount the file system locally for amending
 
     def mountBus(self):
         #Mount over USB
