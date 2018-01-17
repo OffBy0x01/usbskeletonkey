3006605a7c8665b3beddf70a11fb50cbb8f9f594
Michaela <michaelastewart1997@gmail.com>
2018-01-17 12:21:23 +0000

    Network Class

--- framework/networkEmu.py
+++ framework/networkEmu.py
@@ -1,4 +1,16 @@
 import os
 import sys
 
-"""Testing"""
+# Network Emulation - 17th of January 2018
+# by Michaela Stewart and Jonathan Ross
+
+class NetworkObject(object):
+    """ Class for the Network Object """
+
+    def __init__(self, state):
+        self.state = state
+
+    def network_state(self):
+        # retrieves the current state of the network and displays it (debug)
+        print NetworkObject.state()
+
