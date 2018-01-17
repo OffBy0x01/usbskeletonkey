cae0d4cd172739374e82ab1f21f573fc18c7f895
forbes <1500812@abertay.ac.uk>
2018-01-17 12:47:16 +0000

    File moved

---
+++ framework/storageEmu.py
@@ -0,0 +1,10 @@
+'''
+This file is to load a file system onto the connected host.
+
+This file will have an accompanying file system to clone for each run.
+
+This file system could also be loaded via loopback to save the outputs of modules
+'''
+
+
+import FwComponent
