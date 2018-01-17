0f83d69d03c306f2c7746733c174e31563eecd9d
Michaela <michaelastewart1997@gmail.com>
2018-01-17 12:27:25 +0000

    Network Class with added methods

--- framework/networkEmu.py
+++ framework/networkEmu.py
@@ -3,35 +3,16 @@ import os
 import sys
 import subprocess
 
-
-#USB OTG requirements
-gether = "modprobe g_ether idVendor=0x04b3 idProduct=0x4010"
-gether_up = "ifup usb0"
-gether_remove = "modprobe -r g_ether"
-
-
-def network_on(on):
-
-def network_up(up):
-
-def network_down(down):
-
-def network_remove(remove):
-
-
-
-
-
-
-
-
-
-
 # Network Emulation - 17th of January 2018
 # by Michaela Stewart and Jonathan Ross
 
 class NetworkObject(object):
     """ Class for the Network Object """
+
+    # USB OTG requirements
+    gether = "modprobe g_ether idVendor=0x04b3 idProduct=0x4010"
+    gether_up = "ifup usb0"
+    gether_remove = "modprobe -r g_ether"
 
     def __init__(self, state):
         self.state = state
@@ -39,4 +20,19 @@ class NetworkObject(object):
     def network_state(self):
         # retrieves the current state of the network and displays it (debug)
         print NetworkObject.state()
+
+    def network_on(self):
+        # pass gether to bash
+        pass
+
+    def network_initialise(self):
+        pass
+
+    def network_down(self):
+        pass
+
+    def network_remove(self):
+        pass
+
+
 
