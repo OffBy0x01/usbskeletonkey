55c1122bd2cc0a1fbb7659a18c5406ea7b5fda77
Andrew Calder <Androbcal@gmail.com>
2018-01-18 22:57:57 +0000

    fixed self added separate module and main paths

--- skeleton-key.py
+++ skeleton-key.py
@@ -7,20 +7,25 @@ class skeleton-key():
 
     #This hasn't been updated yet so ignore the init for now.
     def __init__(self):
+        """This is the docstring you fuck"""
+
+        '''
         #initialize framework components
         interface = framework.ui()
         keyboard = framework.keyboard()
         network = framework.network()
         storage = framework.storage()
         io = framework.io()
+        '''
 
-        #get directory and check modules folder exists
-        dir_path = os.path.dirname(os.path.realpath(__file__))
+        self.module_list = self.discover_modules()
+        self.main_path = os.path.dirname(os.path.realpath(__file__))
+        self.module_path = self.main_path + "/modules/"
 
-    def discover_modules(dir_path):
+    def discover_modules(self):
         #get the module paths from modules directory
         print("Looking for modules...")
-        module_paths = os.listdir(dir_path+"/modules")
+        module_paths = os.listdir(self.module_path)
 
         #regex to look for .py files
         py = re.compile("\.py", re.IGNORECASE)
@@ -28,43 +33,40 @@ class skeleton-key():
 
         #identify module name from file path
         identify_module = lambda m: os.path.splitext(m)[0]
-        return ([identify_module(m) for m in module_paths])
+        return [identify_module(m) for m in module_paths]
 
-    def get_modules():
-        if not module_list:
-            module_list = discover_modules()
+    def get_modules(self):
+        module_list = self.discover_modules()
+        return module_list
 
+    def init_config(self):
 
-    def init_config():
-
-        if not (os.path.exists(dir_path+"/modules")):
+        if not (os.path.exists(self.module_path)):
             #doesn't exist - user can fix themselves
-            print("Error: "+dir_path+"\"modules\" directory does not exist!")
+            print("Error: ", self.module_path, " directory does not exist!")
             exit()
 
-        present_modules = get_modules(dir_path)
-
         #if no modules present exit
-        if not present_modules:
+        if not self.module_list:
             print("Error: No modules found!")
             exit()
         else:
             print('Configuring modules:')
-            print(*present_modules, sep=',\n')
+            print(*self.module_list, sep=',\n')
 
         #generate configuration file and add the required sections
         config = configparser.ConfigParser()
-        config.read(dir_path+'/config.ini')
+        config.read(self.main_path + '/config.ini')
         config.add_section('components')
         config.add_section('modules')
         config.add_section('interface')
         config.add_section('general')
 
         #add to modules to config.ini:active_modules
-        for mod in present_modules:
+        for mod in self.module_list:
             config.set('modules', mod, "True")
 
-        with open(dir_path+'/config.ini', 'w') as f:
+        with open(self.main_path + '/config.ini', 'w') as f:
             config.write(f)
 
         print('Done!')
