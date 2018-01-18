25a4b89de57a19fb5790c627b25bb735b5182705
yeroc_sebrof <1500812@abertay.ac.uk>
2018-01-18 22:49:46 +0000

    Pepe changes

--- skeleton-key.py
+++ skeleton-key.py
@@ -3,48 +3,46 @@ import os
 import re
 
 
-class skeleton-key():
+class SkeletonKey:
 
-    #This hasn't been updated yet so ignore the init for now.
+    # This hasn't been updated yet so ignore the init for now.
     def __init__(self):
-        #initialize framework components
+        # initialize framework components
         interface = framework.ui()
         keyboard = framework.keyboard()
         network = framework.network()
         storage = framework.storage()
         io = framework.io()
 
-        #get directory and check modules folder exists
+        # get directory and check modules folder exists
         dir_path = os.path.dirname(os.path.realpath(__file__))
 
-    def discover_modules(dir_path):
-        #get the module paths from modules directory
+    def discover_modules(self, dir_path):
+        # get the module paths from modules directory
         print("Looking for modules...")
         module_paths = os.listdir(dir_path+"/modules")
 
-        #regex to look for .py files
+        # regex to look for .py files
         py = re.compile("\.py", re.IGNORECASE)
         module_paths = filter(py.search, module_paths)
 
-        #identify module name from file path
+        # identify module name from file path
         identify_module = lambda m: os.path.splitext(m)[0]
         return ([identify_module(m) for m in module_paths])
 
-    def get_modules():
+    def get_modules(self):
         if not module_list:
             module_list = discover_modules()
 
-
-    def init_config():
-
-        if not (os.path.exists(dir_path+"/modules")):
-            #doesn't exist - user can fix themselves
-            print("Error: "+dir_path+"\"modules\" directory does not exist!")
+    def init_config(self):
+        if not (os.path.exists(self.dir_path+"/modules")):
+            # doesn't exist - user can fix themselves
+            print("Error: "+self.dir_path+"\"modules\" directory does not exist!")
             exit()
 
-        present_modules = get_modules(dir_path)
+        present_modules = self.get_modules()
 
-        #if no modules present exit
+        # if no modules present exit
         if not present_modules:
             print("Error: No modules found!")
             exit()
@@ -52,15 +50,15 @@ class skeleton-key():
             print('Configuring modules:')
             print(*present_modules, sep=',\n')
 
-        #generate configuration file and add the required sections
+        # generate configuration file and add the required sections
         config = configparser.ConfigParser()
-        config.read(dir_path+'/config.ini')
+        config.read(self.dir_path+'/config.ini')
         config.add_section('components')
         config.add_section('modules')
         config.add_section('interface')
         config.add_section('general')
 
-        #add to modules to config.ini:active_modules
+        # add to modules to config.ini:active_modules
         for mod in present_modules:
             config.set('modules', mod, "True")
 
@@ -68,4 +66,3 @@ class skeleton-key():
             config.write(f)
 
         print('Done!')
-
