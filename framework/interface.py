f2f9ebe08368e24a5bf1b0aa620392cc330a9996
Michaela <michaelastewart1997@gmail.com>
2018-01-18 21:32:54 +0000

    fixed all print statements - only need to fix global variables issue cause im a silly-billy

--- framework/interface.py
+++ framework/interface.py
@@ -5,16 +5,26 @@ import sys
 
 
 class ModuleObjects(object):
-    """ Class for Module Objects"""
+    """
+     Class for the Module Object
+
+         Args:
+            name:           name of the modules
+            nes_modules:    list of module indexes needed to run the current module
+
+        functions:
+            none:           currently
+
+        Returns:
+            module list
+
+        Raises:
+            none currently.
+    """
 
     def __init__(self, name, nes_modules):
         self.name = name
         self.nes_modules = nes_modules
-
-
-module_1 = ModuleObjects("Responder", 1)
-module_2 = ModuleObjects("NMap", 12)
-module_3 = ModuleObjects("Enumeration", 23)
 
 
 class InterfaceObject(object):
@@ -26,50 +36,50 @@ class InterfaceObject(object):
         self.modules = modules
 
     def display_title(self):
-        print InterfaceObject.title
+        print(InterfaceObject.title)
 
     def display_modules(self):
-        if range(len(self.modules)) == 0:
-            print "There are no modules to display."
+        if not self.modules:
+            print("There are no modules to display.")
         else:
             for module in range(len(self.modules)):
-                print (module + 1), " ", self.modules[module]
+                print(module + 1), " ", self.modules[module]
 
 
 # Main Program
 # Load in Module file
 test_file = ["Responder", "NMap", "Enumeration"]
 intro = InterfaceObject(test_file)
-
-intro = InterfaceObject(test_file)
 exit_flag = False
 
-while exit_flag != True:
+while not exit_flag:
     # clear the screen - os.system('clear')
     # Display title
     intro.display_title()
     # Display modules
     intro.display_modules()
 
-    print " "
-    print "Enter 0 to exit"
-    user_selection = int(raw_input("Please enter the module you would like to configure. (Based on index)"))
+    print("\n")
+    print("Enter 0 to exit")
+    user_selection = int(input("Please enter the module you would like to configure. (Based on index)"))
     if user_selection == 0:
-        print "Exiting Program..."
+        print("Exiting Program...")
         exit_flag = True
         pass
     if user_selection == str:
-        print "Error 101: Invalid selection - string instead of integer."
+        print("Invalid selection - string instead of integer.")
         pass
     elif user_selection < 0 or user_selection > len(intro.modules):
-        print "Error 102: Invalid index selection. Please enter a valid selection."
+        print("Invalid index selection. Please enter a valid selection.")
         pass
     else:
-        if exit_flag != True:
+        if not exit_flag:
             current_module = test_file[(user_selection - 1)]
             # mainly for debug
-            print "Running ", current_module, "..."
+            print("Running ", current_module, "...")
             pass
         else:
-            print "Thank you for using 'Skeleton Key'."
+            print("Thank you for using 'Skeleton Key'.")
             sys.exit(0)
+
+
