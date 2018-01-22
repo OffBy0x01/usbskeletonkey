b6689f0d9dab2d1f446ce7abcc2fd8fc2b4b181e
jonathan <jdjr98@gmail.com>
2018-01-22 14:35:17 +0000

    Added ping function to check whether internet access is possible after enabling the adapter.

--- framework/network.py
+++ framework/network.py
@@ -1,13 +1,11 @@
 import subprocess
 import time
+import os
 
 from framework import FwComponentGadget
 
 
-# -*- coding: utf-8 -*-
-
-
-class fw_component_network(FwComponentGadget):
+class FwComponentNetwork(FwComponentGadget):
     """ Class for the Network Object
 
          Args:
@@ -17,6 +15,9 @@ class fw_component_network(FwComponentGadget):
         functions:
             enable:         allows for enabling of driver
             disable:        allows for disabling of driver
+            network_on:     allows for the ethernet adapter to be turned on
+            network_off:    allows for the ethernet adapter to be turned off
+            network_remove: allows for disable() to be called to disable and remove the driver
 
         Returns:
             framework component object
@@ -32,25 +33,41 @@ class fw_component_network(FwComponentGadget):
         self.ether_down = "ifdown usb0"
         super().enable()  # Initialising Ethernet
         self.state = "initialised"
+        self.ping_address = "8.8.8.8"
+        self.ping_response = ""
+
+    def __del__(self):
+        self.network_remove()
 
     # Turning on USB Ethernet adapter
     def network_on(self):
         subprocess.call("%s" % self.ether_up, shell=True)
         self.state = "Ethernet adapter up"
+        self.ping_test()
 
     # Turning off USB Ethernet adapter
-    def network_down(self):
+    def network_off(self):
         subprocess.call("%s" % self.ether_down, shell=True)
         self.state = "Ethernet adapter down"
-
-    # Deconstructor
-    def __del__(self):
-        self.network_remove()
 
     # Removing USB Ethernet
     def network_remove(self):
         super().disable()
         self.state = "uninitialised"
+
+    def ping_test(self):
+        self.ping_response = subprocess.call("ping -c 1 " + self.ping_address)
+        if self.ping_response == 0:
+            super().debug("ping successful!")
+            return
+        else:
+            super().debug("Ping unsuccessful!")
+
+
+
+
+
+
 
 
 
