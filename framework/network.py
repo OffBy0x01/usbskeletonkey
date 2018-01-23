5f8bf62093665e498f9445d534ee7702c2f29826
jonathan <jdjr98@gmail.com>
2018-01-23 10:39:15 +0000

    Pushing for Ellis to be working on same code. Attempting to add kill switch.

--- framework/network.py
+++ framework/network.py
@@ -1,4 +1,5 @@
 import subprocess
+import time
 
 from framework.FwComponentGadget import FwComponentGadget
 
@@ -33,38 +34,59 @@ class FwComponentNetwork(FwComponentGadget):
         self.state = "initialised"
         self.ping_address = "8.8.8.8"
         self.ping_response = ""
+        self.timer_start = time.time()
+        self.timer_end = ""
 
     def __del__(self):
         self.network_remove()
 
     # Turning on USB Ethernet adapter
     def network_on(self):
+
         subprocess.call("%s" % self.ether_up, shell=True)
         self.state = "Ethernet adapter up"
+        if self.debug:
+            super().debug(self.state)
         self.ping_test()
+        return
 
     # Turning off USB Ethernet adapter
     def network_off(self):
         subprocess.call("%s" % self.ether_down, shell=True)
         self.state = "Ethernet adapter down"
+        if self.debug:
+            super().debug(self.state)
+        time.sleep(9)
+        self.timer_end = time.time()
+        print (self.timer_end - self.timer_start)
+        return
 
     # Removing USB Ethernet
     def network_remove(self):
         super().disable()
         self.state = "uninitialised"
+        if self.debug:
+            super().debug(self.state)
+        return
 
+    # Check for internet connectivity
     def ping_test(self):
         self.ping_response = subprocess.call("ping -c 1 " + self.ping_address)
         if self.ping_response == 0:
             super().debug("ping successful!")
-            return
+
         else:
             super().debug("Ping unsuccessful!")
+        return
+
+    #def kill_switch(self):
+# if time > 5 mins:
+    # do kill switch
 
 
 
-
-
+test = FwComponentNetwork()
+test.network_off()
 
 
 
