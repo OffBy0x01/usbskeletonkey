e4d0e6ee367527c9e5624ba32bdd2c68f82fa878
Andrew Calder <Androbcal@gmail.com>
2018-01-23 01:37:36 +0000

    Added ModuleManager, moved discover_modules to it

---
+++ framework/helper/ModuleManager.py
@@ -0,0 +1,26 @@
+
+from framework.helper.singleton import Singleton
+import re
+import os
+
+@Singleton
+class ModuleManager:
+
+    def __init__(self):
+        self.module_list = self.discover_modules()
+
+    def discover_modules(self):
+        # get the module paths from modules directory
+        print("Looking for modules...")
+        module_paths = os.listdir(self.module_path)
+
+        # regex to look for .py files
+        py = re.compile("\.py", re.IGNORECASE)
+        module_paths = filter(py.search, module_paths)
+
+        # identify module name from file path
+        return [os.path.splitext(m)[0] for m in module_paths]
+
+        # .__thisclass__ == current class
+        # .__self_class__ == class of self argument (usually the child)
+        # https://stackoverflow.com/questions/3277367/how-does-pythons-super-work-with-multiple-inheritance
