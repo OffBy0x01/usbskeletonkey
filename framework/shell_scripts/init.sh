9ee788c9aea928f7c23bf749e9915f266505dfc4
EGRichmond <ellisg.richmond@outlook.com>
2018-01-30 15:37:26 +0000

    fixed file path for dhcpd.conf

--- framework/shell_scripts/init.sh
+++ framework/shell_scripts/init.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-cp ../dhcpd.conf /etc/dhcp/dhcpd.conf  # Copy DHCP configs
+cp ../../dhcpd.conf /etc/dhcp/dhcpd.conf  # Copy DHCP configs
 echo -e 'iface usb0 inet static\naddress 10.10.10.10\nnetmask 128.0.0.0\ngateway 10.10.10.1' >> /etc/network/interfaces  # Set static IP
