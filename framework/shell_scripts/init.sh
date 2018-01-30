98ac24441be2ebb562a3b1320dac7936d23261c6
EGRichmond <ellisg.richmond@outlook.com>
2018-01-30 15:39:05 +0000

    Added stop for DHCP server

--- framework/shell_scripts/init.sh
+++ framework/shell_scripts/init.sh
@@ -2,3 +2,4 @@
 
 cp ../../dhcpd.conf /etc/dhcp/dhcpd.conf  # Copy DHCP configs
 echo -e 'iface usb0 inet static\naddress 10.10.10.10\nnetmask 128.0.0.0\ngateway 10.10.10.1' >> /etc/network/interfaces  # Set static IP
+/etc/init.d/isc-dhcp-server stop  # Ensure DHCP server is stopped
