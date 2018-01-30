11d9fe5c71a0722ad4a44baa8ac76e236920e222
EGRichmond <ellisg.richmond@outlook.com>
2018-01-30 18:04:53 +0000

    Installer for python 3.6.4

---
+++ framework/shell_scripts/install_python.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+# Taken from here (https://gist.github.com/dschep/24aa61672a2092246eaca2824400d37f)
+# Install build tools
+apt-get update
+apt-get install build-essential tk-dev libncurses5-dev libncursesw5-dev libreadline6-dev libdb5.3-dev libgdbm-dev libsqlite3-dev libssl-dev libbz2-dev libexpat1-dev liblzma-dev zlib1g-dev
+
+wget https://www.python.org/ftp/python/3.6.4/Python-3.6.4.tar.xz
+tar xf Python-3.6.4.tar.xz
+cd Python-3.6.4
+./configure
+make
+sudo make altinstall
+
