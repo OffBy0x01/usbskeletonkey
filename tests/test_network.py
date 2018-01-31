34fc28e993c047f031ae4f33ddb5d6adb15aec5b
AR-Calder <Androbcal@gmail.com>
2018-01-31 04:13:49 +0000

    Added tests for components
    Signed-off-by: AR-Calder <Androbcal@gmail.com>

--- tests/test_network.py
+++ tests/test_network.py
@@ -1,3 +1,9 @@
+import os.path
+import sys
+
+# Required at top of file to allow testing
+sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
+
 from components.framework.network import FwComponentNetwork
 
 # For testing
