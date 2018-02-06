1b2858e47e21254fa6bc917b76d35d25733f28bf
EGRichmond <ellisg.richmond@outlook.com>
2018-02-06 11:04:35 +0000

    config loading updated

--- components/framework/shell_scripts/init.sh
+++ components/framework/shell_scripts/init.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
-cp ../../dhcpd.conf /etc/dhcp/dhcpd.conf  # Copy DHCP configs
-# Do the same for /etc/dhcpcd.conf and /etc/network/interfaces
+
 /etc/init.d/isc-dhcp-server stop  # Ensure DHCP server is stopped
+
+cp ../../configs/dhcpd.conf /etc/dhcp/dhcpd.conf  # Copy DHCP configs
+cp ../../configs/dhcpcd.conf /etc/dhcpcd.conf
+cp ../../configs/interfaces /etc/network/interfaces  # Copy interfaces
