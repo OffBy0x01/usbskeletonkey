899f67f2512650ad74b32125a57cd40e30607bd0
AR-Calder <Androbcal@gmail.com>
2018-02-08 21:40:51 +0000

    Added missing usb0 conf for install requirements
    Signed-off-by: AR-Calder <Androbcal@gmail.com>

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -3,7 +3,7 @@
 sudo apt-get update
 sudo apt-get install rpi-update python git python-dev  python-pip sqlite3 isc-dhcp-server python-crypto inotify-tools screen
 
-if ! sudo uname -a | grep -q "4.4.50"; then
+if ! sudo uname -a | grep -q "4.4.50+"; then
 	sudo BRANCH=master rpi-update 5224108
 	sudo reboot now	
 fi
@@ -13,7 +13,7 @@ sudo touch /boot/ssh
 if sudo grep -q "usb0" /etc/network/interfaces; then
 	echo "usb0 already configured"
 else
-	cat /home/pi/usbskeletonkey/usb0-config | sudo tee --append /etc/network/interfaces > /dev/null
+	cat /home/pi/usbskeletonkey/config/usb0-config | sudo tee --append /etc/network/interfaces > /dev/null
 fi
 
 if sudo grep -q "dtoverlay=dwc2" /boot/config.txt; then
