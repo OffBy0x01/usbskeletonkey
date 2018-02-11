d4117f8f5e0a5e3791eedd8b5b57f722d518643d
EGRichmond <ellisg.richmond@outlook.com>
2018-02-11 11:21:50 +0000

    Fixed broke file paths

--- components/framework/shell_scripts/init.sh
+++ components/framework/shell_scripts/init.sh
@@ -2,6 +2,6 @@
 
 /etc/init.d/isc-dhcp-server stop  # Ensure DHCP server is stopped
 
-cp ../../configs/dhcpd.conf /etc/dhcp/dhcpd.conf  # Copy DHCP configs
-cp ../../configs/dhcpcd.conf /etc/dhcpcd.conf
-cp ../../configs/interfaces /etc/network/interfaces  # Copy interfaces
+cp ../../../configs/dhcpd.conf /etc/dhcp/dhcpd.conf  # Copy DHCP configs
+cp ../../../configs/dhcpcd.conf /etc/dhcpcd.conf
+cp ../../../configs/interfaces /etc/network/interfaces  # Copy interfaces
