6e1ea3e0b3251d16b0d40d3c5b64ddfd9591c2c4
EGRichmond <ellisg.richmond@outlook.com>
2018-02-13 10:46:44 +0000

    Added line to copy isc-dhcp-server config

--- components/framework/shell_scripts/setup.sh
+++ components/framework/shell_scripts/setup.sh
@@ -16,6 +16,7 @@ cp ../../../config/interfaces /etc/network/interfaces
 cp ../../../config/dhcpcd.conf /etc/dhcpcd.conf
 cp ../../../config/resolv.conf /etc/resolv.conf
 cp ../../../config/dhcpd.conf /etc/dhcp/dhcpd.conf
+cp ../../../config/isc-dhcp-server /etc/default/isc-dhcp-server
 
 ifdown wlan0
 ifup wlan0
