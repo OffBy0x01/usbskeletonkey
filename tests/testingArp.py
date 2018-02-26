983990a94019438ac9df6f187a3213c25d22adeb
Corey <1500812@abertay.ac.uk>
2018-02-26 14:57:46 +0000

    Minor check changes

--- tests/testingArp.py
+++ tests/testingArp.py
@@ -5,7 +5,7 @@ import sys
 sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
 from components.modules.arp import ipIsValid, arpScan
 
-IPs = ["192.168.0.0", "192.168.1.0/24"]
+IPs = ["192.168.0.0/24", "192.168.1.0/24"]
 # IP = "127.0.0.1"
 
 #print(ipIsValid(<test>)
