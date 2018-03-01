d3324a166946b62f507cce9bfed609adfef2d872
AR-Calder <Androbcal@gmail.com>
2018-03-01 04:12:37 +0000

    Added IP and Port exclusion

--- components/helpers/IpValidator.py
+++ components/helpers/IpValidator.py
@@ -2,32 +2,34 @@ import socket
 
 # From https://stackoverflow.com/a/4017219
 
-def is_valid_ipv4_address(address):
-    try:
-        socket.inet_pton(socket.AF_INET, address)
-    except AttributeError:
+
+class IpValidator:
+    def is_valid_ipv4_address(address):
         try:
-            socket.inet_aton(address)
+            socket.inet_pton(socket.AF_INET, address)
+        except AttributeError:
+            try:
+                socket.inet_aton(address)
+            except socket.error:
+                return False
+
+            return address.count('.') == 3
+
+        # Address invalid
         except socket.error:
             return False
 
-        return address.count('.') == 3
-
-    # Address invalid
-    except socket.error:
-        return False
-
-    # Address is valid
-    return True
+        # Address is valid
+        return True
 
 
-def is_valid_ipv6_address(address):
-    try:
-        socket.inet_pton(socket.AF_INET6, address)
+    def is_valid_ipv6_address(address):
+        try:
+            socket.inet_pton(socket.AF_INET6, address)
 
-    # Address invalid
-    except socket.error:
-        return False
+        # Address invalid
+        except socket.error:
+            return False
 
-    # Address is valid
-    return True
+        # Address is valid
+        return True
