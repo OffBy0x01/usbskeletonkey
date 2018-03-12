2eae855108edc6816a827cfc3c10423b5f759143
yeroc_sebrof <1500812@abertay.ac.uk>
2018-03-12 13:26:42 +0000

    Refactored variable names

--- components/modules/tracert.py
+++ components/modules/tracert.py
@@ -8,31 +8,32 @@ import subprocess
 import re
 
 
-def traceRoute(target, interface="usb0", bypassRoutingTables=False, hopBackChecks=True, mapHostNames=True, originalOut=False):
+def traceRoute(target, interface="usb0", bypass_routing_tables=False, hop_back_checks=True,
+               map_host_names=True, original_out=False):
     """
     Makes use of the traceroute command.
     No default flags are in use that the user cannot access via output
 
     Args:
-    :param target: takes in a IPv4 target
+    :param target: takes in a IPv4 target (Cannot Take a list)
     :param interface: Defaults to usb0 but can make use of any interface that is available
-    :param bypassRoutingTables: Allows for traceroute to take the most direct approach bypassing routing tables
-    :param hopBackChecks: Confirms that packets taken by the response follow the same path
-    :param mapHostNames: In the event that mapping host names to IP makes noise this can be disabled
-    :param originalOut: If the user wants the original command output this should be changed to true
+    :param bypass_routing_tables: Allows for traceroute to take the most direct approach bypassing routing tables
+    :param hop_back_checks: Confirms that packets taken by the response follow the same path
+    :param map_host_names: In the event that mapping host names to IP makes noise this can be disabled
+    :param original_out: If the user wants the original command output this should be changed to true
 
     :return: list of ip lists for each hop. Often single item list but keeps consistent for accessing
     """
     command = ["traceroute", "-i", interface]  # start with command items that are required
 
     # Add command arguments where appropriate
-    if bypassRoutingTables:
+    if bypass_routing_tables:
         command += ["-r"]
 
-    if hopBackChecks:
+    if hop_back_checks:
         command += ["--back"]
 
-    if not mapHostNames:
+    if not map_host_names:
         command += ["-n"]
 
     if type(target) is str:
@@ -44,7 +45,7 @@ def traceRoute(target, interface="usb0", bypassRoutingTables=False, hopBackCheck
     # Running command
     output = subprocess.run(command, stdout=subprocess.PIPE).stdout.decode("utf-8")
 
-    if originalOut is True:  # If user doesnt want output parsed
+    if original_out is True:  # If user doesnt want output parsed
         return output
 
     # Parsing output
@@ -54,7 +55,7 @@ def traceRoute(target, interface="usb0", bypassRoutingTables=False, hopBackCheck
 
     output_out = []
 
-    if mapHostNames:
+    if map_host_names:
         for line in output:
             results = []  # init var to store current results
             line = line.split()
@@ -95,16 +96,16 @@ def ipIsValid(IP, iprange=False):
     :return: boolean indicating if the IP is valid, True for valid IP
     """
     # Side note, might need IPv6 support. TODO Check this isn't an issue
-    ipRange = "(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)"  # This checks a number is within 0-255
-    anIPv4 = ipRange + "\." + ipRange + "\." + ipRange + "\." + ipRange  # This regex will check its a IP
+    ip_range = "(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)"  # This checks a number is within 0-255
+    an_ipv4 = ip_range + "\." + ip_range + "\." + ip_range + "\." + ip_range  # This regex will check its a IP
 
-    anIPv4Range = anIPv4 + "\/[0-2][0-9]|" + anIPv4 + "\/3[0-2]"  # This checks IP ranges such as 192.168.0.0/24
+    an_ipv4_range = an_ipv4 + "\/[0-2][0-9]|" + an_ipv4 + "\/3[0-2]"  # This checks IP ranges such as 192.168.0.0/24
     # The checks with this one are more lax. Still error prone
 
     if iprange:
-        check = re.search("\A" + anIPv4 + "\Z|\A" + anIPv4Range + "\Z", IP)
+        check = re.search("\A" + an_ipv4 + "\Z|\A" + an_ipv4_range + "\Z", IP)
     else:
-        check = re.search("\A" + anIPv4 + "\Z", IP)
+        check = re.search("\A" + an_ipv4 + "\Z", IP)
 
     if check is None:
         return False
