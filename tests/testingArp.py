e7cf1754e694ecdebff08280b6c12971f0dc2377
yeroc_sebrof <1500812@abertay.ac.uk>
2018-03-20 14:07:22 +0000

    Refactored fro implementation to Enumerate
    Signed-off-by: yeroc_sebrof <1500812@abertay.ac.uk>

--- tests/testingArp.py
+++ tests/testingArp.py
@@ -3,13 +3,14 @@ import sys
 
 # Required at top of file to allow testing
 sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
-from components.modules.arp import ipIsValid, arpScan
+from components.modules.arp import is_valid_ipv4_address, get_targets_via_arp
 
-IPs = ["192.168.0.0/24", "192.168.1.0/24"]
+IPs = ["192.168.1.0"]
+# IPs = ["10.0.0.0/8", "172.16.0.0/12", "192.168.0.0/16"]
 # IP = "127.0.0.1"
 
-#print(ipIsValid(<test>)
+#print(is_valid_ipv4_address(<test>)
 
-output = arpScan(IPs, interface="wlp4s0b1")
+output = get_targets_via_arp(IPs, interface="wlp4s0b1")
 
 print(output)
--- tests/testingFPing.py
+++ tests/testingFPing.py
@@ -8,11 +8,11 @@ from components.modules.fping import *
 # IPs = ["192.168.0.0/24", "192.168.1.0/24"]
 # IP = "127.0.0.1"
 
-# print(ipIsValid("192.168.0.0/24"))
+# print(is_valid_ipv4_address("192.168.0.0/24"))
 
 # Currently unable to get good output from host names, Tempted to void this functionality
-
-output = fping("www.google.com", get_ips_from_dns=True)
+'''
+output = check_target_is_alive("www.google.com", get_ips_from_dns=True, get_dns_name=True)
 
 print(output)
 
@@ -24,10 +24,19 @@ print(output)
 
 pings = 4
 
-output = fping("8.8.8.8", ping_count=pings)
+output = check_target_is_alive("8.8.8.0/28", ping_count=pings, interface="wlp4s0b1")
 
-if pings > 0:
-    output = output.split(" : ")
+output = output.split("\n")
+output.pop()
 
-print(output)
-'''
+output_final = []
+
+if type(output) is list:
+    for item in output:
+        if pings > 0:
+            output_final += [item.split(" : ")]
+    print(output_final)
+else:
+    if pings > 0:
+        output = output.split(" : ")
+    print(output)
