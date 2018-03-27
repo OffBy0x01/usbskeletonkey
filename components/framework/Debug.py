3262a07eaafbb75c4e7dadd5306b9f816f0dd9f5
Andrew Calder <Androbcal@gmail.com>
2018-03-27 13:45:03 +0100

    Added separate module debug mode
    Signed-off-by: Andrew Calder <Androbcal@gmail.com>

--- components/framework/Debug.py
+++ components/framework/Debug.py
@@ -4,6 +4,7 @@ class Debug(object):
     def __init__(self, name="debug", type="component", debug=False):
         self._debug = debug
         self._name = name
+        self._module = ""
         self._type = type
         self._operations = 0
         self._successful_operations = 0
@@ -13,7 +14,13 @@ class Debug(object):
 
     def debug(self, txt=""):
         if self._debug:
-            print(self._type, '/', self._name, ': ', txt, sep="")
+            print(self._type, '/', self._name, self._module, ': ', txt, sep="")
+
+    def enable_module_debug(self, module_name):
+        self._module = "/" + module_name
+
+    def disable_module_debug(self):
+        self._module = ""
 
     def action(self, outcome=True):
         """
