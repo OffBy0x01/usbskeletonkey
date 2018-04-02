edd3fb689543d07ec1ebf250c95ea287666d51db
yeroc-sebrof <1500812@abertay.ac.uk>
2018-04-02 19:49:13 +0100

    Added methods
    Signed-off-by: yeroc-sebrof <1500812@abertay.ac.uk>

--- components/helpers/BlinktSupport.py
+++ components/helpers/BlinktSupport.py
@@ -6,18 +6,37 @@ class Blinkt:
     This is a simple class for modules to make use of blinkt if they wish
     """
     def __init__(self, red=30, green=30, blue=30, brightness=0.1):
-        self.colors = [red, green, blue]
+        self.colours = [red, green, blue]
         set_brightness(brightness)
 
     def __exit__(self):
-        clear()
-        show()
+        self.clear()
 
-    def setPixel(self, pixel):
-        set_pixel(pixel, self.colors[0], self.colors[1], self.colors[2])
+    @staticmethod
+    def unset_pixel(pixel):
+        set_pixel(pixel, 0, 0, 0)
         show()
         return
 
-    def newColors(self, red, green, blue):
-        self.colors = [red, green, blue]
+    def set_pixel(self, pixel):
+        set_pixel(pixel, self.colours[0], self.colours[1], self.colours[2])
+        show()
+        return
+
+    @staticmethod
+    def clear():
+        clear()
+        show()
+
+    @staticmethod
+    def new_brightness(brightness):
+        set_brightness(brightness)
+        return
+
+    def new_colours(self, red, green, blue):
+        self.colours = [red, green, blue]
+        return
+
+    def new_colors(self, red, green, blue):
+        self.new_colours(red, green, blue)
         return
