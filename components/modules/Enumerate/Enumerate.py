ce8ea62ee8a60de066f0e45e3d0b4fb2d385964f
EGRichmond <ellisg.richmond@outlook.com>
2018-04-03 16:12:22 +0100

    Data check for tracert

--- components/modules/Enumerate/Enumerate.py
+++ components/modules/Enumerate/Enumerate.py
@@ -153,8 +153,8 @@ class Enumerate():
 
             arp_response_valid = False  # Flag for valid data
             if arp_response:  # If list of data exists
-                for x in arp_response:  # Check each item in list for valid data
-                    if x:  # If data exists
+                for item in arp_response:  # Check each item in list for valid data
+                    if item:  # If data exists
                         arp_response_valid = True  # Set flag to true
                         break
 
@@ -173,16 +173,20 @@ class Enumerate():
             current_route_valid = False  # Flag for valid data
             if current.ROUTE:  # If list exists
                 if current.ROUTE[0]:  # Check first list for data
-                    current_route_valid = True
-                    break
-                if current.ROUTE[1]:  # Check second list of lists
-                    for x in current.ROUTE[1]:  # Check each list
-                        if x:  # If data exists
+                    for item in current.ROUTE[0]:
+                        if item:
                             current_route_valid = True
                             break
+                if current.ROUTE[1]:  # Check second list of lists
+                    for item in current.ROUTE[1]:  # Check each list of lists
+                        if item:  # If data exists
+                            for sub_item in item:  # Check each list
+                                if sub_item:  # If data exists
+                                    current_route_valid = True
+                                    break
 
-            if not current_route_valid:
-                current.ROUTE = False
+            if not current_route_valid:  # If no valid data was found
+                current.ROUTE = False  # Set value to false
 
             # use all port scanning tools against current ip
             for port in self.port_list:
