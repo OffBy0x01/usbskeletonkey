2c74b2d397d387ab47710df4d7d827c0d5b5dc62
yeroc-sebrof <1500812@abertay.ac.uk>
2018-04-03 14:25:32 +0100

    Testing the method within Enumerate
    Signed-off-by: yeroc-sebrof <1500812@abertay.ac.uk>

--- tests/testingFPing.py
+++ tests/testingFPing.py
@@ -1,16 +1,16 @@
-import os.path
+import os
 import sys
 
 # Required at top of file to allow testing
 sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
-from components.modules.fping import *
+from components.modules.Enumerate.Enumerate import Enumerate
 
 IPs = "127.0.0.0"
 # IP = "127.0.0.1"
 
-output = check_target_is_alive(IPs,
-                               interface="wlp4s0b1",
-                               ping_count=4,
-                               verbose=True)
+output = Enumerate.check_target_is_alive(IPs,
+                                         interface="wlp4s0b1",
+                                         ping_count=4,
+                                         verbose=True)
 
 print(output)
