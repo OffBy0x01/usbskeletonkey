1bbd22e79c7cb10a64aab01c8f20c831d34a8921
Joh98 <jdjr98@gmail.com>
2018-04-03 17:08:47 +0100

    Quick test case to separate each item in the list that NMAP in enumerate.py creates.

---
+++ tests/test_NMAP_list_parsing.py
@@ -0,0 +1,10 @@
+
+
+nmap_list = [[['23', '', '', 'closed'], ['34', '', '', 'closed'], ['44', '', '', 'closed']], ['Linux 4.4 (94%)',
+      'Linux 3.5 (91%)', 'Linux 4.2 (91%)', 'Synology DiskStation Manager 5.1 (91%)', 'Linux 3.18 (90%)',
+      'Linux 2.6.35 (90%)']]
+
+for position in nmap_list:
+    for value in position:
+        print(value)
+
