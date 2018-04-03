4e244c6c92746259eeaf4e42f080efc4c75a82a1
Joh98 <jdjr98@gmail.com>
2018-04-03 17:59:51 +0100

    Added pretty colours to debug messages.

--- components/framework/network.py
+++ components/framework/network.py
@@ -2,6 +2,7 @@ import subprocess
 
 from components.framework.FwComponentGadget import FwComponentGadget
 from components.framework.Debug import Debug
+from components.helpers.Color import Color
 
 
 class FwComponentNetwork(FwComponentGadget):
@@ -53,7 +54,7 @@ class FwComponentNetwork(FwComponentGadget):
                 flag_success = True
                 break
             else:  # If ping not successful
-                self.network.debug("Ping unsuccessful!")
+                self.network.debug("Ping unsuccessful!", color=Color.WARNING)
                 # Try again
         if not flag_success:  # If 3 ping attempts fail
             return self.kill("Connection failed!")
@@ -64,54 +65,54 @@ class FwComponentNetwork(FwComponentGadget):
         self.enable()
 
         usb0_ifup = subprocess.call("ifup usb0", shell=True)
-        self.network.debug("Failed to ifup usb0" if usb0_ifup else "usb0 ifup successful")  # Up usb0 interface
+        self.network.debug(Color.FAIL + "Failed to ifup usb0" + Color.DEFAULT if usb0_ifup else "usb0 ifup successful")  # Up usb0 interface
         if usb0_ifup:  # If process failed return False
             return False
 
         usb0_ifconfig = subprocess.call("ifconfig usb0 up", shell=True)
-        self.network.debug("Failed to up networking on usb0" if usb0_ifconfig else "usb0 networking up")  # Up networking on usb0
+        self.network.debug(Color.FAIL + "Failed to up networking on usb0" + Color.DEFAULT if usb0_ifconfig else "usb0 networking up")  # Up networking on usb0
         if usb0_ifconfig: # If process failed return False
             return False
 
         usb0_routes = subprocess.call("/sbin/route add -net 0.0.0.0/0 usb0", shell=True)
-        self.network.debug("Failed to add IP routes to usb0" if usb0_routes else "usb0 IP routes added successfully")  # Add route for all IPv4 addresses
+        self.network.debug(Color.FAIL + "Failed to add IP routes to usb0" + Color.DEFAULT if usb0_routes else "usb0 IP routes added successfully")  # Add route for all IPv4 addresses
         if usb0_routes: # If process failed return False
             return False
 
         dhcp = subprocess.call("/etc/init.d/isc-dhcp-server start", shell=True)
-        self.network.debug("Failed to start DHCP server" if dhcp else "DHCP server successfully started")  # Start DHCP server
+        self.network.debug(Color.FAIL + "Failed to start DHCP server" + Color.DEFAULT if dhcp else "DHCP server successfully started")  # Start DHCP server
         if dhcp: # If process failed return False
             return False
 
         ip_forwarding = subprocess.call("/sbin/sysctl -w net.ipv4.ip_forward=1", shell=True)
-        self.network.debug("Failed to enable IPv4 forwarding" if ip_forwarding else "IPv4 forwarding successfully enabled")  # Enable IPv4 forwarding
+        self.network.debug(Color.FAIL + "Failed to enable IPv4 forwarding" + Color.DEFAULT if ip_forwarding else "IPv4 forwarding successfully enabled")  # Enable IPv4 forwarding
         if ip_forwarding:
             return False
 
         bind = subprocess.call("/sbin/iptables -t nat -A PREROUTING -i usb0 -p tcp --dport 80 -j REDIRECT --to-port 1337", shell=True)
-        self.network.debug("Failed to bind port 80 to 1337" if bind else "Successfully binded port 80 to port 1337")  # Bind port 80 to port 1337
+        self.network.debug(Color.FAIL + "Failed to bind port 80 to 1337" + Color.DEFAULT if bind else "Successfully binded port 80 to port 1337")  # Bind port 80 to port 1337
         if bind: # If process failed return False
             return False
 
         dnsspoof = subprocess.call("/usr/bin/screen -dmS dnsspoof /usr/sbin/dnsspoof -i usb0 port 53", shell=True)
-        self.network.debug("Failed to start dnsspoof on port 53" if dnsspoof else "Successfully started dnsspoof on port 53")  # Start dnsspoof on port 53
+        self.network.debug(Color.FAIL + "Failed to start dnsspoof on port 53" + Color.DEFAULT if dnsspoof else "Successfully started dnsspoof on port 53")  # Start dnsspoof on port 53
         if dnsspoof:
             return False
 
         self.state = "usb0 should be up"
         if self.network.debug:  # Debug text
-            self.network.debug(self.state)
+            self.network.debug(self.state, color=Color.OKGREEN)
         return True
 
     # Turning off USB Ethernet adapter
     def down(self):
-        self.network.debug("Failed to disable IPv4 forwarding" if subprocess.call("/sbin/sysctl -w net.ipv4.ip_forward=0", shell=True) else "IPv4 forwarding successfully disabled")  # Disable IPv4 forwarding
-        self.network.debug("Failed to stop DHCP server" if subprocess.call("/etc/init.d/isc-dhcp-server stop", shell=True) else "DHCP server successfully stopped")  # Stop DHCP server
-        self.network.debug("Failed to remove IP routes from usb0" if subprocess.call("/sbin/route del -net 0.0.0.0/0 usb0", shell=True) else "usb0 IP routes removed successfully")  # Remove route for all IPv4 addresses
+        self.network.debug(Color.FAIL + "Failed to disable IPv4 forwarding" + Color.DEFAULT if subprocess.call("/sbin/sysctl -w net.ipv4.ip_forward=0", shell=True) else "IPv4 forwarding successfully disabled")  # Disable IPv4 forwarding
+        self.network.debug(Color.FAIL + "Failed to stop DHCP server" + Color.DEFAULT if subprocess.call("/etc/init.d/isc-dhcp-server stop", shell=True) else "DHCP server successfully stopped")  # Stop DHCP server
+        self.network.debug(Color.FAIL + "Failed to remove IP routes from usb0" + Color.DEFAULT if subprocess.call("/sbin/route del -net 0.0.0.0/0 usb0", shell=True) else "usb0 IP routes removed successfully")  # Remove route for all IPv4 addresses
 
         # Down adapter
-        self.network.debug("Failed to down networking on usb0" if subprocess.call("ifconfig usb0 down", shell=True) else "usb0 networking down")
-        self.network.debug("Failed to ifdown usb0" if subprocess.call("ifdown usb0", shell=True) else "usb0 ifdown successful")
+        self.network.debug(Color.FAIL + "Failed to down networking on usb0" + Color.DEFAULT if subprocess.call("ifconfig usb0 down", shell=True) else "usb0 networking down")
+        self.network.debug(Color.FAIL + "Failed to ifdown usb0" + Color.DEFAULT if subprocess.call("ifdown usb0", shell=True) else "usb0 ifdown successful")
 
         # Debug
         self.state = "usb0 down"
@@ -119,6 +120,6 @@ class FwComponentNetwork(FwComponentGadget):
 
  # Emergency Kill
     def kill(self, error_message):
-        super().debug(error_message)  # Debug text
+        super().debug(error_message, color=Color.FAIL)  # Debug text
         self.disable()  # Detach from bus
         return False
