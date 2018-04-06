480200297f114c695b883985ef7fcbe16fcd69f4
yeroc-sebrof <1500812@abertay.ac.uk>
2018-04-06 15:17:52 +0100

    Optional Blinkt flag
    Signed-off-by: yeroc-sebrof <1500812@abertay.ac.uk>

--- components/modules/Enumerate/Enumerate.py
+++ components/modules/Enumerate/Enumerate.py
@@ -143,12 +143,19 @@ class Enumerate:
         # ---------------------
         target_ips = defaultdict()  # Init of dictionary
 
-        blinkt = Blinkt(255, 0, 0)
+        if self.current_config.options['use_blinkt'].lower() == 'true':
+            blinkt = Blinkt(int(self.current_config.options['rcolor']),
+                            int(self.current_config.options['gcolor']),
+                            int(self.current_config.options['bcolor']))
 
         current_ip_in_list = 0
+
         for ip in self.ip_list_shuffled:  # Make it less obvious
 
-            blinkt.progressive_pixels(current_ip_in_list, len(self.ip_list_shuffled))
+            try:
+                blinkt.progressive_pixels(current_ip_in_list, len(self.ip_list_shuffled))
+            finally:
+                self.enumerate.debug("Target " + current_ip_in_list + " of " + len(self.ip_list_shuffled))
 
             current = TargetInfo()
 
