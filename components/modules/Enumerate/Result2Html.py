0840cc91addd9f4698a01451bfb35a9012bf339b
Andrew Calder <Androbcal@gmail.com>
2018-04-09 05:33:50 +0100

    SHARE_INFO
    Signed-off-by: Andrew Calder <Androbcal@gmail.com>

--- components/modules/Enumerate/Result2Html.py
+++ components/modules/Enumerate/Result2Html.py
@@ -251,7 +251,7 @@ class Result2Html:
                                             with tag('th'):
                                                 text("Comment")
                                     with tag('tbody'):
-                                        for index in range(len(targets[IP].LOCAL)):
+                                        for index in range(len(targets[IP].SHARE_INFO)):
                                             with tag('tr'):
                                                 with tag('td'):
                                                     text(targets[IP].LOCAL[x][0])
